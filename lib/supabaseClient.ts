2d7517febede5060e1d70e2b55bb5f8e3c2a2f02
Alex Thomas <48531362+AlexThomas-Graft0@users.noreply.github.com>
2026-07-05 08:27:40 +0100

    add lib/supabaseClient.ts

---
+++ lib/supabaseClient.ts
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+  { db: { schema: "tat_2d8c65" } },
+);
